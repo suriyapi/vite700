218987811053a67ab6e09587ee90e2939432687f
suriyapi <suriya.p@ku.th>
2025-03-02 15:48:32 +0700

    Experience update

---
+++ src/contents/experience.js
@@ -0,0 +1,39 @@
+import { faGithub, faMedium, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import picExp from '../assets/ex1.png'
+
+export const data = [
+    {
+        date: "2020-2021",
+        title: "Fullstackkk · Sport Analysis System",
+        link: "https://suriyapi.github.io/vite/",
+        materials: [
+            { type: faGithub, link: "https://suriyapi.github.io/vite/" },
+            { type: faYoutube, link: "https://suriyapi.github.io/vite/" },
+            { type: faMedium, link: "https://suriyapi.github.io/vite/" },
+        ],
+        descriptions: [
+            "Designing and developing a sports data analysis system that scrapes data from various sports websites, processes it, and presents different profit-making possibilities in real-time for customers.",
+            "Create tools for monitoring the backend and implementing self-recovery mechanisms, ensuring their seamless operation 24/7.",
+        ],
+        skills: [
+            ["React", "Redus", "MUI"],
+            ["NginX", "NodeJS", "Express", "MongoDB"],
+        ],
+        picture: picExp,
+    },
+    {
+        date: "2022-2023",
+        title: "Fullstack · Sport Analysis System",
+        link: "https://suriyapi.github.io/vite/",
+        materials: [
+            { type: faGithub, link: "https://suriyapi.github.io/vite/" },
+        ],
+        descriptions: [
+            "Create tools for monitoring the backend and implementing self-recovery mechanisms, ensuring their seamless operation 24/7.",
+        ],
+        skills: [
+            ["React", "Redus", "MUI"],
+        ],
+        picture: picExp,
+    },
+]
--- src/sections/Experience/index.jsx
+++ src/sections/Experience/index.jsx
@@ -1,77 +1,63 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faMedium, faYoutube } from '@fortawesome/free-brands-svg-icons';
-import picExp from '../../assets/ex1.png';
-import picExp2 from '../../assets/ex2.png';
 import { useState } from 'react';
 import FormattedDate from '../../components/FormattedDate';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import Picture from '../../components/Picture';
 import TitleLink from '../../components/TitleLink';
 import Material from '../../components/Material';
 import Description from '../../components/Description';
 import Tech from '../../components/Tech';
+import { data } from '../../contents/experience';
+
+const SECTION_ID = 'experience-section';
+
 const Experience = () => {
     const [isMouseEnter, setIsMouseEnter] = useState({});
     return (
-    <div className="">
+    <div id={SECTION_ID}>
         <div className="text-primaryAccent font-medium">Experience</div>
-        <div 
-            className={`grid grid-cols-[25%_75%] rounded-md px-2 py-6 transition-all ${isMouseEnter ['ex1'] ? "bg-gray-200":""}`}
-            onMouseEnter={() => setIsMouseEnter({['ex1']: true})}
-            onMouseLeave={() => setIsMouseEnter({['ex1']:false})}
-        >
-            <div>
-                <FormattedDate isHighLight = {isMouseEnter ['ex1']}>2023-2024</FormattedDate>
-                <Picture picture={picExp} title='Fashion Ecommerce'/>
-            </div>
-            <div className="grid gap-y-4">
-                <TitleLink isHighLight = {isMouseEnter ['ex1']} title='Fashion Ecommerce' link="https://www.google.com/"/>
-                
-                <div className='flex gap-4 text-xl'>
-                    <Material icon={faGithub} link="https://www.google.com/" />
-                    <Material icon={faMedium} link="https://www.google.com/" />
-                    <Material icon={faYoutube} link="https://www.google.com/" />
-                </div>
-
-                <Description description="Designing and developing a sports data analysis system that scrapes data from various sports websites."/>
-                
-                <div >
-                    <Tech isHighLight = {isMouseEnter ['ex1']} data={['React', 'Node.js', 'Express']}/>
-                </div>
-            </div>
-        </div>
-        
-        <div 
-            className={`grid grid-cols-[25%_75%] rounded-md px-2 py-6 transition-all ${isMouseEnter ['ex2'] ? "bg-gray-200":""}`}
-            onMouseEnter={() => setIsMouseEnter({['ex2']: true})}
-            onMouseLeave={() => setIsMouseEnter({['ex2']:false})}
-        >
-            <div>
-                <div>
-                    <span className={`text-sm ${isMouseEnter ['ex2'] ?"text-primaryTitle":""}`}>2022-2023</span>
-                </div>
-                <div>
-                    <img src={picExp2} className='w-5/6 rounded-md' />
-                </div>
-            </div>
-            <div className="grid gap-y-4">
-                <div className={`text-primaryAccent ${isMouseEnter ['ex2'] ?"text-primaryTitle":""}`}>
-                    Fashion Ecommerce
-                    <FontAwesomeIcon className={`text-xs -rotate-45 duration-500 ease-out ${isMouseEnter ['ex2'] ? "translate-x-1 -translate-y-1":""}`} icon={faArrowRight}/>
-                </div>
-                <div className='flex gap-4 text-xl'>
-                    <a href='https://www.google.com/' target="_blank" className='hover:scale-110'><FontAwesomeIcon icon={faGithub}/></a>
-                    <a href='https://www.google.com/' target="_blank" className='hover:scale-110'><FontAwesomeIcon icon={faMedium}/></a>
-                    <a href='https://www.google.com/' target="_blank" className='hover:scale-110'><FontAwesomeIcon icon={faYoutube}/></a>
-                </div>
-                <div className='text-sm'>Designing and developing a sports data analysis system that scrapes data from various sports websites.</div>
-                <div className="flex gap-4 text-sm">
-                    <div className={`bg-primarySubcontent px-2 py-1 rounded-md ${isMouseEnter ['ex2'] ? "text-primaryTitle" : ""} `}>React</div>
-                    <div className={`bg-primarySubcontent px-2 py-1 rounded-md ${isMouseEnter ['ex2'] ? "text-primaryTitle" : ""} `}>Node.js</div>
-                    <div className={`bg-primarySubcontent px-2 py-1 rounded-md ${isMouseEnter ['ex2'] ? "text-primaryTitle" : ""} `}>Express</div>
-                </div>
-            </div>
-        </div>
+        {
+            data.map(({
+                date,
+                title,
+                link,
+                materials,
+                descriptions,
+                skills,
+                picture,
+            }, index) => (
+                    <div 
+                        key={`${SECTION_ID}-${index}-${title.replaceAll(" ", "")}`}
+                        className={`grid grid-cols-[25%_75%] rounded-md px-2 py-6 transition-all ${isMouseEnter [`${SECTION_ID}-${index}`] ? "bg-gray-200":""}`}
+                        onMouseEnter={() => setIsMouseEnter({[`${SECTION_ID}-${index}`]: true})}
+                        onMouseLeave={() => setIsMouseEnter({[`${SECTION_ID}-${index}`]:false})}
+                    >
+                        <div>
+                            <FormattedDate isHighLight = {isMouseEnter [`${SECTION_ID}-${index}`]}>{date}</FormattedDate>
+                            <Picture picture={picture} title={title}/>
+                        </div>
+                        <div className="grid gap-y-4">
+                            <TitleLink isHighLight = {isMouseEnter [`${SECTION_ID}-${index}`]} title={title} link={link}/>
+                            
+                            <div className='flex gap-4 text-xl'>
+                                {
+                                    materials.map((e,i) => (
+                                        <Material key={`${e}-material-${i}`} icon={e.type} link={e.link} />
+                                    ))
+                                }
+                            </div>
+                            {
+                                descriptions.map((e,i) => (
+                                    <Description key={`${e}-descriptions-${i}`} description={e}/>
+                                ))
+                            }
+                            {
+                                skills.map((e,i) => (
+                                    <Tech key={`${e}-skill-${i}`} isHighLight = {isMouseEnter [`${SECTION_ID}-${index}`]} data={e}/>
+                                ))
+                            }
+                        </div>
+                    </div>
+           )) 
+        }
     </div>
     )
 }
